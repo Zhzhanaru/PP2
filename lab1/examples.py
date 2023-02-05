4eea7fd4d4535c0cb05d8e50cd8774a0bdad4bd6
Zhanaru Manap <112745374+Zhzhanaru@users.noreply.github.com>
2023-02-05 14:11:21 +0600

    Add files via upload

---
+++ lab1/examples.py
@@ -0,0 +1,86 @@
+#intro
+print("Hello,World!")
+#syntax
+if 5>2:
+    print("five is greater than two")
+#creating comment
+print("Zhanaru")#comment
+
+#variables
+x=5
+y="Zhanaru"
+print(x)
+print(y)
+print(x,y)
+
+z=20
+z="Zhanaru"
+print(z)
+
+#variables:casting
+a=str(3)
+b=int(5)
+c=float(3)
+print(a,b,c)
+#variables:get the type
+q=5
+r="Zhanaru"
+print(type(q),type(r))
+#single-double quotes
+n='John'
+y="Zhanaru"
+print(n,y)
+#case sensitive
+e=2
+E="ZHanaru"
+print(e,E)
+#list
+fruits=["apple","banaana","orange"]
+m,b,v=fruits
+print(m,b,v)
+#output variables
+t="This "
+h="is "
+i="an "
+s="apple"
+print(t+h+i+s)
+
+#global
+l = "awesome"
+
+def func():
+  print("Python is " + l)
+
+func()
+
+#DATATYPES
+x="hello"#str
+x=20#int
+x=20.5#float
+x=1j#complex
+x=["apple","juice"]#list
+x=("apple","juice")#tuple
+x=range(6)#range
+x={"name":"John","age":"20"}#dict
+x={"apple","bananas"}#set
+x=frozenset({"apple","bananas"})#frozenset
+x=True#bool
+x=b"hello"#bytes
+x=bytearray(5)#bytearray
+x=memoryview(bytes(5))#memoryview
+x=None#Nonetypes
+
+
+#Casting
+x=int(1)#->1
+y=int(3.2)#->3
+z=("3")#->3
+
+
+#strings
+print("jello")
+#print('hee')
+a="hello"
+print(a[1])#->it shows "e"
+
+
